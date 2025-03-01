77037a13f68e344ceb2c83dd69e1b5f5e6cfd18d
cedricahenkorah <cedricahenkorah@gmail.com>
2025-03-01 13:49:08 +0000

    feat: add login and signup buttons to the sidebar layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { ToggleSidebar } from "@/components/sidebar/toggle-sidebar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,8 +34,21 @@ export default function RootLayout({
       >
         <SidebarProvider defaultOpen={false}>
           <AppSidebar />
-          <SidebarInset className="p-2">
-            <ToggleSidebar />
+          <SidebarInset className="p-4">
+            <div className="flex justify-between items-center">
+              <ToggleSidebar />
+
+              <div className="flex items-center gap-x-2">
+                <Button asChild>
+                  <Link href="/auth">Log in</Link>
+                </Button>
+
+                <Button asChild variant="outline">
+                  <Link href="/auth">Sign up</Link>
+                </Button>
+              </div>
+            </div>
+
             {children}
           </SidebarInset>
         </SidebarProvider>
